16450f40616b1cae1e8a6ea191bad4edc6f009ab
alejoab1990 <alejoab12@hotmail.com>
2020-03-01 16:56:13 -0500

    Se ajustan fuentes nuevas

---
+++ src/main/java/com/toures/balon/TouresBalonApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.toures.balon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouresBalonApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouresBalonApiGatewayApplication.class, args);
+	}
+
+}
