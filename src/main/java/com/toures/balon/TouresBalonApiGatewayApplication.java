4f7af6e5cee3e78150c0fec914d412240ece1e14
malcalab <malcalab@everis.com>
2020-03-02 20:54:02 -0500

    se imprimen configuraciones

--- src/main/java/com/toures/balon/TouresBalonApiGatewayApplication.java
+++ src/main/java/com/toures/balon/TouresBalonApiGatewayApplication.java
@@ -1,13 +1,29 @@
 package com.toures.balon;
 
+import java.lang.invoke.MethodHandles;
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.config.GatewayProperties;
 
 @SpringBootApplication
 public class TouresBalonApiGatewayApplication {
-
+	@Autowired
+	private GatewayProperties props;
+	
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	public static void main(String[] args) {
 		SpringApplication.run(TouresBalonApiGatewayApplication.class, args);
+	}
+	@PostConstruct
+	public void init() {
+		logger.info(Objects.toString(props));
 	}
 
 }
